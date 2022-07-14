f50b820550d04d73c755619d5b4bd8b8f8080260
Beth-Anne Farmer <bethannef@protonmail.com>
2022-07-14 14:33:29 -0400

    new stuff

--- App.js
+++ App.js
@@ -1,14 +1,59 @@
 
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
+
+import { LinearGradient } from 'expo-linear-gradient';
+
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
-import { LinearGradient } from 'expo-linear-gradient';
+
 import Colors from './constants/colors';
+
+
+
+
 export default function App() {
-const [userNumber, setUserNumber] = useState();
-const [gameIsOver, setGameIsOver] = useState(true);
+  const [ appIsReady, setAppIsReady ] = useState(false);
+  const [ userNumber, setUserNumber ] = useState();
+  const [ gameIsOver, setGameIsOver ] = useState(true);
+
+  const fontsLoading = {
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+ };
+
+useEffect(() => {
+  async function prepare() {
+     try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync(fontsLoading);
+     } catch (e) {
+        console.warn(e);
+     } finally {
+        setAppIsReady(true);
+     }
+  }
+
+  prepare();
+}, []);
+
+const onLayoutRootView = useCallback(
+  async () => {
+     if (appIsReady) {
+        await SplashScreen.hideAsync();
+     }
+  },
+  [ appIsReady ]
+);
+
+if (!appIsReady) {
+  return null;
+}
+
 function pickedNumberHandler(pickedNumber) {
   setUserNumber(pickedNumber);
   setGameIsOver(false);
@@ -30,11 +75,12 @@ if (gameIsOver && userNumber) {
 
 
   return (
-  <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
+  <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}   onLayout={onLayoutRootView}>
  <ImageBackground source={require('./assets/images/background.png')}
                      resizeMode="cover"
                      style={styles.rootScreen}
                      imageStyle={styles.backgroundImage}
+                  
  >
   <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
   </ImageBackground>
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import { TextInput, View, StyleSheet, Alert } from 'react-native';
+import { TextInput, View, StyleSheet, Alert} from 'react-native';
 import Colors from '../constants/colors';
 import PrimaryButton from '../components/ui/PrimaryButton';
-
+import Title from '../components/ui/Title';
+import Card from '../components/ui/Card';
+import InstructionText from '../components/ui/InstructionText';
 function StartGameScreen({onPickNumber}) {
 const [enteredNumber, setEnteredNumber] = useState('');
 
@@ -30,7 +32,11 @@ function confirmInputHandler() {
 
 }
 
-    return <View style={styles.inputContainer}>
+    return (
+  <View style={styles.rootContainer}>
+<Title>Guess My Number</Title>
+    <Card>
+        <InstructionText>Enter a Number</InstructionText>
         <TextInput 
         style={styles.numberInput} 
         maxLength={2} 
@@ -48,25 +54,19 @@ function confirmInputHandler() {
         <PrimaryButton onPress1={confirmInputHandler}>Confirm</PrimaryButton>
         </View>
         </View>
-    </View>
+    </Card>
+   </View>
+    
+    )
 }
 
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
+    rootContainer: {
+        flex: 1,
         marginTop: 100,
-        marginHorizontal: 24,
-        padding: 16,
-        backgroundColor: Colors.primary800,
-        borderRadius: 8,
-        elevation: 4,
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.25
+        alignItems: 'center',
     },
     numberInput: {
         height: 50,
